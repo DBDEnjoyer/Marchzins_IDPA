0834b129422d4438ca57e9842276daed2f88ca21
DBDEnjoyer <110892739+DBDEnjoyer@users.noreply.github.com>
2025-08-26 13:55:09 +0200

    Create Program.py

---
+++ Program.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+def berechne_marchzins(kapital: float, zinssatz: float, geburtstag: str) -> float:
+    """
+    Berechnet den Marchzins-Bonus.
+    
+    :param kapital: Sparkapital in CHF
+    :param zinssatz: Zinssatz in Prozent (z.B. 1.5)
+    :param geburtstag: Geburtstag im Format 'TT.MM'
+    :return: Marchzins-Bonus in CHF
+    """
+    tag, monat = map(int, geburtstag.split("."))
+    # Anzahl Tage vom 1. bis zum Geburtstag
+    tage = tag  
+    
+    # Berechnung mit Bankjahr (360 Tage)
+    zins = (kapital * zinssatz * tage) / (360 * 100)
+    return round(zins, 2)
+
+# Beispiel
+print(berechne_marchzins(10000, 1.5, "15.03"))  # -> 6.25 CHF
